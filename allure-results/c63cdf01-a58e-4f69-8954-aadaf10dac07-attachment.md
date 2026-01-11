951c3016f6d9e98d347faff53030e1b9d606aa73
Ayush Singh <as.singh1411@gmail.com>
2026-01-11 09:24:03 +0530

    new changes in js

--- allure-results/c63cdf01-a58e-4f69-8954-aadaf10dac07-attachment.md
+++
@@ -1,9 +0,0 @@
-# Page snapshot
-
-```yaml
-- generic [active] [ref=e1]:
-  - heading "Not Found" [level=1] [ref=e2]
-  - paragraph [ref=e3]: The requested URL was not found on this server.
-  - separator [ref=e4]
-  - generic [ref=e5]: Apache/2.4.41 (Ubuntu) Server at opensource-demo.orangehrmlive.com Port 443
-```
---
+++ tests/register/register.spec.js
@@ -0,0 +1,16 @@
+import { Register } from '../../page/registerpage';
+import { test } from '@playwright/test';
+
+test.describe("Register form", () => {
+    let reg;
+
+    test.beforeEach(async ({ page }) => { 
+        reg = new Register({ page });  // matches constructor
+        await reg.open();
+    });
+
+    test("To verify the click signup and login", async ({ page }) => {
+        await reg.verifySignupLogin();
+        await reg.verifysignupoff();
+    });
+});
